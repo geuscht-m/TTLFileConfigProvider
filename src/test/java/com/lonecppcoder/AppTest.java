4302cccd0c1cb46033d1d34d9c96a1083a6cef88
Timo Geusch <timo.geusch@mongodb.com>
2022-06-07 10:48:39 -0400

    Import of base FileConfigProvider
    - Imported Apache Kafka FileConfigProvider as starting point
    - Create Maven project
    - Added dependencies

---
+++ src/test/java/com/lonecppcoder/AppTest.java
@@ -0,0 +1,20 @@
+package com.lonecppcoder;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
